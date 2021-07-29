10e6ee8a86ac69dd4200740471929f7f9dce5d9a
helllo-ai <71385601+helllo-ai@users.noreply.github.com>
2021-07-29 07:55:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask,jsonify,request
+from data import Data
+app=Flask(__name__)
+@app.route("/")
+def index():
+    name=request.args.get("name")
+    planet_data=next(item for item in data if item["name"]==name)
+    return jsonify({
+        "data":data,
+        "message":"success"
+    }),200
+if __name__=="__main__":
+    app.run()
